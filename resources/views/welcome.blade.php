0917b81eed6cc8c00f1d0ab8747d245c8f601184
Linards <linards_lazdins@hotmail.com>
2020-12-13 08:48:14 +0200

    Added docker config

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,8 @@
             <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                 <a href="{{ url('/') }}" class="text-sm text-gray-700 underline">Home</a>
                 <a href="{{ url('/admin') }}" class="text-sm text-gray-700 underline">Voyager Admin</a>
-            </div>
+            </div>                
+            {{-- @endif --}}
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                     <svg viewBox="0 0 651 192" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-16 w-auto text-gray-700 sm:h-20">
